2543211f7f5abd07a405698af4678d97c5709f0c
Tim Gymnich <TG908@users.noreply.github.com>
2019-07-25 01:12:54 +0200

    Added documentation and a way to link a type string from a url to a swift type

--- Sources/OTPKit/Account.swift
+++ Sources/OTPKit/Account.swift
@@ -6,15 +6,36 @@
 //
 
 import Foundation
+import KeychainAccess
 
-
-struct Account {
+struct Account: Codable {
+    /// The label is used to identify which account a key is associated with
     var label: String
+    /// OTP instance used by the account. Responsible for all cryptograhic operations.
     var otp: OTP
+    /// String identifying the provider or service managing that account
     var issuer: String?
+    /// URL refering to the image for the account.
     var imageURL: URL?
-
+    /// All the account information encoded in a otpauth URL
+    var url: URL {
+        let queryItemImageURL = URLQueryItem(name: "image", value: imageURL?.absoluteString)
+        // otpauth://TYPE/ISSUER:LABEL?PARAMETERS
+        guard var components = URLComponents(string: "otpauth://\(type(of: otp).otpType)/\(issuer ?? "")\(issuer == nil ? "" : ":")\(label)") else {
+            fatalError("Error encoding URL")
+        }
+        var queryItems = [queryItemImageURL] + otp.urlQueryItems
+        // remove query items with no value (optional parameters)
+        queryItems = queryItems.filter { $0.value != nil }
+        components.queryItems = queryItems
+        return components.url!
+    }
     
+    
+    /// - Parameter label: The label is used to identify which account a key is associated with. It contains an account name, which is a URI-encoded string, optionally prefixed by an issuer string identifying the provider or service managing that account. This issuer prefix can be used to prevent collisions between different accounts with different providers that might be identified using the same account name, e.g. the user's email address.
+    /// - Parameter otp: OTP instance used by this account.
+    /// - Parameter issuer: The issuer parameter is a string value indicating the provider or service this account is associated with, URL-encoded according to RFC 3986. If the issuer parameter is absent, issuer information may be taken from the issuer prefix of the label. If both issuer parameter and issuer label prefix are present, they should be equal.
+    /// - Parameter imageURL: URL refering to the image for the account.
     init(label: String, otp: OTP, issuer: String? = nil, imageURL: URL? = nil) {
         self.label = label
         self.otp = otp
@@ -22,6 +43,9 @@ struct Account {
         self.imageURL = imageURL
     }
     
+    
+    /// Used to initalize a account from a URL.
+    /// - Parameter url: A url encoded like this: otpauth://TYPE/ISSUER:LABEL?PARAMETERS
     init?(from url: URL) {
         // otpauth://TYPE/LABEL?PARAMETERS
         
@@ -33,7 +57,6 @@ struct Account {
         
         let components = url.pathComponents.dropFirst().first?.split(separator: ":")
         
-                
         guard let label = components?.last else {
             return nil
         }
@@ -47,16 +70,42 @@ struct Account {
             self.imageURL = URL(string: imageURLString)
         }
         
-        switch type {
-        case "totp":
-            guard let totp = TOTP(from: url) else { return nil }
-            self.otp = totp
-        case "hotp":
-            guard let hotp = HOTP(from: url) else { return nil }
-                self.otp = hotp
-        default:
-            return nil
+        guard let otp = OTPType(for: type)?.implementation.init(from: url) else { return nil }
+        self.otp = otp
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let url = try container.decode(URL.self)
+        self.init(from: url)!
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(url)
+    }
+    
+    
+    /// Saves the account to a keychain
+    /// - Parameter keychain
+    func save(to keychain: Keychain) throws {
+        try keychain
+            .label(label)
+            .set(url.absoluteString, key: "otpauth")
+    }
+    
+    
+    /// Loads all the accounts from a keychain
+    /// - Parameter keychain
+    static func load(from keychain: Keychain) throws -> [Account] {
+        let items = keychain.allKeys()
+        let accounts = try items.map { key throws -> Account in
+            let urlString = try keychain.get(key)!
+            let url = URL(string: urlString)!
+            let account = Account(from: url)!
+            return account
         }
+        return accounts
     }
     
 }
--- Sources/OTPKit/OTP.swift
+++ Sources/OTPKit/OTP.swift
@@ -8,16 +8,53 @@
 import Foundation
 import CommonCrypto
 
+enum OTPType: String, Codable, CaseIterable {
+    // Main purpose of this enum is to link a type string from a url to the Swift type implementing that OTP variant.
+    
+    // The type string as it would appear in the URL:
+    // otpauth://TYPE/LABEL?PARAMETERS
+    case hotp = "hotp"
+    case totp = "totp"
+    
+    /// Type conforming to OTP that implements the OTPType
+    var implementation: OTP.Type {
+        switch self {
+        case .hotp: return HOTP.self
+        case .totp: return TOTP.self
+        }
+    }
+    
+    init?(for type: String) {
+        // There can only be one or zero matching types because strings in a string backed enum must be unique.
+        guard let matchingType = OTPType.allCases.filter({ $0.rawValue == type }).first else {
+            return nil
+        }
+        self = matchingType
+    }
+}
 
 protocol OTP: Codable {
+    /// Algorithm used to calculate the hash
     var algorithm: Algorithm { get }
-    var secret: Data { get set }
+    /// The secret is an arbitrary key value
+    var secret: Data { get }
+    /// The digits parameter determines how long of a one-time passcode to display to the user. The default is 6.
     var digits: Int { get }
-    var count: UInt64 { get }
+    /// Used to derive to one time password
+    var counter: UInt64 { get }
     
+    /// Used to match the OTP type string in a URL to determine the matching swift type.
+    static var otpType: OTPType { get }
+    /// Used by Account to URL encode the HOTP instance.
+    var urlQueryItems: [URLQueryItem] { get }
+    
+    /// Generate a one time password.
     mutating func code() -> String
+    /// Generate a one time password for a specific counter value.
     func code(for count: UInt64) -> String
     
+    /// Initalizes the OTP instance from a URL
+    /// - Parameter url
     init?(from url: URL)
 }
 
@@ -29,11 +66,13 @@ extension OTP {
         let secretPointer = self.secret.withUnsafeBytes { ptr -> UnsafeRawPointer in
             return ptr.baseAddress!
         }
-            
+        
+        // Perform HMAC
         hash.withUnsafeMutableBytes { ptr in
             CCHmac(algorithm.rawValue, secretPointer, secret.count, &swappedCount, MemoryLayout.size(ofValue: swappedCount), ptr.baseAddress!)
         }
-                
+        
+        // Mask the hash
         let offset = Int(hash[algorithm.hashLength - 1]) & 0x0f
         
         let mask = hash.withUnsafeBytes { ptr -> UInt32 in
@@ -43,7 +82,7 @@ extension OTP {
         
         var divisor: UInt32 = 1
         for _ in 0..<digits { divisor *= 10 }
-                
+        
         return String(format: String(format: "%%0%hhulu", digits), mask % divisor)
     }
 }
