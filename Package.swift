9193ebfb7decdeaae5c4b1a824b7481cbe0c3b29
Tim Gymnich <tg908@icloud.com>
2020-06-27 01:45:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/norio-nomura/Base32.git", from: "0.7.0"),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "3.2.0"),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
